97c85481d553ab682ec79c83bee1bf0d7a1fd63c
cntencra <cn10cra@gmail.com>
2025-07-24 08:44:54 +0100

    feat: quiz page
    added state to update faces to change with correct or wrong answer

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -8,68 +8,104 @@ const insectImgUrls = [
     "/saw-toothed-grain-beetle.png"
 ]
 
+const insectCommonNames = [
+    ["merchant grain beetle"],
+    ["oriental cockroach"],
+    ["sawtooth grain beetle"]
+]
+
+const insectSpecificNames = [
+    "Oryzaephilus mercator",
+    "Blatta orientalis",
+    "Oryzaephilus surinamensis"
+]
+
 const insectIds = [0,1,2]
 
 const faces = [
-    "/happy.svg",
+    "/sad.svg",
     "/nuetral.svg",
-    "/sad.svg"
+    "/happy.svg"
+    
 ]
 
 export default function QuizPage() {
-    const [genericName, setGenericName] = useState("")
-    const [specificName, setSpecificName] = useState("")
-    const [currentID, setCurrentId] = useState(0)
-    const [insectImgUrl, setInsectImgUrl] = useState(insectImgUrls[0])
-    const [clicked, setClicked] = useState(false)
 
-    const handleGenericChange = () => {
-
-    }
-
-    const handleSpecificChange = () => {
-
-    }
+    const [currentInsectId, setCurrentInsectId] = useState(0)
+    const [commonFaceIndex, setCommonFaceIndex] =useState(1)
+    const [specificFaceIndex, setSpecificFaceIndex] =useState(1)
+    const [inputCommonName, setInputCommonName] = useState("")
+    const [inputSpecificName, setInputSpecificName] = useState("")
+   
 
     const handleNext = () => {
-        const oldId = currentID
-        console.log(oldId, 'old id')
-        const indexOfOldId = insectIds.indexOf(oldId)
-        console.log(indexOfOldId, 'index of old id')
-        //remove current insectID from array
-        insectIds.splice(indexOfOldId,1)
-        console.log(insectIds, 'insectIds')
-        // get a random index for the current insectId array
-        const randomIndex = Math.round(Math.random()*(insectIds.length - 1))
-        console.log(randomIndex, 'randomIndex')
-        // set new Img Url with new insectID
-        setInsectImgUrl(insectImgUrls[insectIds[randomIndex]])
-        console.log(insectImgUrl)
-        // set new currentID
-        setCurrentId(() => insectIds[randomIndex])
-        console.log(currentID, 'new current_id \n')
-        // replace old currentId in insectId array
-        insectIds.splice(indexOfOldId,-1,oldId)
-        console.log(insectIds, 'insectIDs replaced')
+        const newId = getNextId()
+        setInputCommonName("")
+        setInputSpecificName("")
+
+        setCurrentInsectId(newId)
     }
 
-    const handleCommon = () => {
-        setClicked(true)
-        alert('You clicked Next!')
+    const getNextId = () => {
+        const oldId = currentInsectId
+        const insectIdsCopy = [...insectIds]
+        const indexOfOldId = insectIdsCopy.indexOf(oldId)
+        insectIdsCopy.splice(indexOfOldId,1)
+        const randomIndex = Math.floor(Math.random()*(insectIdsCopy.length))
+        return insectIdsCopy[randomIndex]
     }
 
-    const handleSpecific = () => {
-        setClicked(true)
-        alert('You clicked Next!')
+    const handleCommonChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const faceIds = insectCommonNames[currentInsectId].map(
+            (commonName) => 
+                getFaceIndex(event.target.value, commonName, true)
+        )
+        const faceId = Math.max(...faceIds)
+        setCommonFaceIndex(faceId)
+        setInputCommonName(event.target.value)
+    }
+
+    const handleSpecificChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+       const facesIndex = getFaceIndex(event.target.value, insectSpecificNames[currentInsectId], false)
+       setSpecificFaceIndex(facesIndex)
+       setInputSpecificName(event.target.value)
+    }
+
+    const getFaceIndex = (input: string, insectName: string, isCommonName: boolean) => {
+        if(input === "") return 1
+        if(fullMatch(input, insectName, isCommonName)) return 2
+        if(partialMatch(input, insectName)) return 1
+        return 0
+    }
+
+    const fullMatch = (input:string, insectName:string, isCommonName: boolean) => {
+        if(insectName === input) return true
+        if(isCommonName && insectName.toLowerCase() === input.toLowerCase()) return true
+        return false
+    }
+
+    const partialMatch = (input:string, insectName:string, ) => {
+        if(insectName.toLowerCase().includes(input.toLowerCase())) return true
+        return false
+    }
+
+
+    const handleCommonAnswer = () => {
+        if(commonFaceIndex == 2) return
+    }
+
+    const handleSpecificAnswer = () => {
+        if(commonFaceIndex == 2) return
     }
 
     return (
         <main className="flex flex-col items-center px-4 pb-5 min-h-screen">
+
             <div id="grid-container"
             className="grid grid-rows-[0.05fr_0.8fr_0.05fr_0.05fr_0.05fr] items-center justify-items-center min-h-screen"
             >
                 <Image
-                    className="w-auto mt-5"
+                    className="w-auto mt-5 dark:invert"
                     priority
                     src='/ntp-title.png'
                     alt="Insect not found"
@@ -79,52 +115,52 @@ export default function QuizPage() {
                 <Image
                     className="w-auto"
                     priority
-                    src={insectImgUrl} 
+                    src={insectImgUrls[currentInsectId]} 
                     alt="Insect not found"
-                    width={120}
-                    height={40}
+                    width={100}
+                    height={300}
                 />
-                <div id="specific-container" className="flex flex-row max-w-screen">
+                <div id="common-container" className="flex flex-row max-w-screen">
                 
                     <button
-                            onClick={handleCommon}
+                            onClick={handleCommonAnswer}
                             className="rounded border border-solid border-transparent bg-red-600 text-white text-xs px-1 mr-1"
                             >
                             Show Answer
                     </button>
                     <input
                         type = "text"
-                        value={genericName}
-                        onChange={handleGenericChange}
-                        placeholder="Generic Name"
+                        value={inputCommonName}
+                        onChange={handleCommonChange}
+                        placeholder="Common Name"
                         className="border-2 rounded  text-center py-2"
                     />
-                    <img
-                        src="/happy.svg" alt="Specific Name is not correct"
-                        className="ml-1"
-                        width={40}
-                        style={{height:"auto"}}
+                    <Image
+                        src={faces[commonFaceIndex]} alt="Specific Name is not correct"
+                        className="ml-1 w-10"
+                        width={10}
+                        height={10}
                     />
                 </div>
                 <div id="specific-container" className="flex flex-row max-w-screen">
                     <button
-                        onClick={handleNext}
+                        onClick={handleSpecificAnswer}
                         className="rounded border border-solid border-transparent  bg-red-600 text-white text-xs px-1 mr-1 mt-1"
                         >
                         Show Answer
                     </button>
                     <input
                         type = "text"
-                        value={specificName}
+                        value={inputSpecificName}
                         onChange={handleSpecificChange}
                         placeholder="Specific Name"
                         className="border-2 rounded  text-center py-2 mt-1"
                     />
-                    <img
-                        src={faces[1]} alt="Specific Name is not correct"
-                        className="ml-1"
-                        width={40}
-                        style={{height:"auto"}}
+                    <Image
+                        src={faces[specificFaceIndex]} alt="Specific Name is not correct"
+                        className="ml-1 w-10"
+                        width={10}
+                        height={10}
                     />
                 </div>
                 <button
