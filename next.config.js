29382af776544a254d54fc30ca8da2350c61452c
cntencra <cn10cra@gmail.com>
2025-09-08 22:35:50 +0100

    fix: add devanimal images to next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 
 module.exports = {
   images: {
-    remotePatterns: [new URL('https://s3ntp12387.s3.eu-west-2.amazonaws.com/images/**')],
+    remotePatterns: [
+      new URL('https://s3ntp12387.s3.eu-west-2.amazonaws.com/images/**'), 
+      new URL ('https://live.staticflickr.com/**'), 
+      new URL ('https://upload.wikimedia.org/**')
+    ],
   },
   allowedDevOrigins: ['http://local-host*']
 }
