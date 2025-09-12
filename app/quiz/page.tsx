db6bf48eeed9e45aa6aece018f136957b40a3528
cntencra <cn10cra@gmail.com>
2025-09-12 22:51:31 +0100

    fix: dark mode pop up

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -35,24 +35,20 @@ export default function Quiz() {
 
 
     if (quizTopic === null) return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-          <div className="bg-white rounded-2xl shadow-xl p-6 w-80 text-center">
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="rounded-2xl shadow-xl p-6 w-80 text-center">
             <h2 className="text-xl font-bold mb-4">Choose a Quiz Topic</h2>
             <p>There are lots of bugs in this quiz. If you would prefer to see nice animals select the animal quiz for developers!</p>
             <div className="flex flex-col gap-3 pt-3">
               <button
                 onClick={() => setQuizTopic("pests")}
-                className={`px-4 py-2 rounded-lg border transition ${
-                  quizTopic === "pests" ? "bg-blue-500 text-white" : "bg-gray-100"
-                }`}
+                className={`px-4 py-2 rounded-lg border hover:shadow-lg dark:hover:shadow-white cursor-pointer transition `}
               >
                 Pests
               </button>
               <button
                 onClick={() => setQuizTopic("devs")}
-                className={`px-4 py-2 rounded-lg border transition ${
-                  quizTopic === "devs" ? "bg-blue-500 text-white" : "bg-gray-100"
-                }`}
+                className={`px-4 py-2 rounded-lg border hover:shadow-lg dark:hover:shadow-white cursor-pointer transition `}
               >
                 Developers
               </button>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -16,14 +16,14 @@ export default function Nav() {
       {/* Burger Icon - Visible on Mobile */}
       <button
         onClick={toggleMenu}
-        className="sm:hidden text-gray-800 hover:text-red-600 focus:outline-none"
+        className="sm:hidden hover:text-red-600 focus:outline-none"
         aria-label="Toggle Menu"
       >
         {isOpen ? <X size={28} /> : <Menu size={28} />}
       </button>
 
       {/* Desktop Nav Links */}
-      <nav className="hidden sm:flex gap-6  text-gray-800 font-medium text-base ">
+      <nav className="hidden sm:flex gap-6  font-medium text-base ">
         <Link href="/" className="hover:text-red-600 transition">
           Home
         </Link>
@@ -40,9 +40,9 @@ export default function Nav() {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
             transition={{ duration: 0.25 }}
-            className="absolute top-10 left-0 w-48 bg-white rounded-md shadow-lg border sm:hidden"
+            className="absolute top-10 left-0 w-48 rounded-md shadow-lg border sm:hidden"
           >
-            <ul className="flex flex-col p-4 gap-4 text-gray-800 text-lg font-medium">
+            <ul className="flex flex-col p-4 gap-4 text-lg font-medium">
               <li>
                 <Link
                   href="/"
